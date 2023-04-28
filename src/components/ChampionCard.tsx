5b9a5aa86fcef4e0434b0966c80f0bc0aac5c1a8
Tabu Hana <taboodog@hotmail.com>
2023-04-28 14:11:45 -0700

    riven-lol-init

---
+++ src/components/ChampionCard.tsx
@@ -0,0 +1,8 @@
+type ChampionCardProps = {
+
+}
+
+const ChampionCard: React.FC<ChampionCardProps> = ({}) => {
+    return (<div>ChampionCard</div>);
+}
+export default ChampionCard;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,50 @@
+import ChampionCard from "../components/ChampionCard";
+
+const Home = () => {
+  return (
+    <>
+    {/* Buttons */}
+    <nav>
+      <button>Patch</button>
+      <button>Contribute</button>
+    </nav>
+
+    {/* General Tips */}
+    <aside>
+      <div>
+        <h2>General Tips</h2>
+        <ul>
+          <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</li>
+          <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</li>
+          <li>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</li>
+        </ul>
+      </div>
+    </aside>
+    
+    {/* Pages Nav */}
+    <aside>
+      <div>Runes</div>
+      <div>Items</div>
+    </aside>
+
+    {/* Champion MU Card */}
+    <main>
+      <div>
+        <form>
+          <label htmlFor="text">Search</label>
+          <input type="text" />
+        </form>
+      </div>
+      <div>
+        <div>Hard</div>
+        <div>Even</div>
+        <div>Easy</div>
+      </div>
+      <div>
+        <ChampionCard />
+      </div>
+    </main>
+    </>
+  )
+}
+export default Home
