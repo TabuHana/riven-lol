e92f08af4fd235205257f84596cdea7e21be0edc
Tabu Hana <taboodog@hotmail.com>
2023-04-28 16:50:04 -0700

    bg

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,19 @@ import Home from './pages/Home';
 function App() {
 	return (
 		<>
-			<Home />
+			{/* <div style={{ position: 'relative', width: '100%', height: '100%' }}> */}
+			{/* </div> */}
+			<div className='background-image-container'>
+				<div className='bg-container'>
+					<img className='background-image' src='../public/Riven_23.jpg' />
+				</div>
+				<div className='gradient-container'>
+					<div className='gradient'></div>
+				</div>
+			</div>
+			<div className='container'>
+				<Home />
+			</div>
 		</>
 	);
 }
